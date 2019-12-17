60bf6470b800a7cb1ca356189254eb05a1c9ca7a
x86nick <x86nick@gmail.com>
2019-12-17 09:57:49 -0800

    Create switch.go

---
+++ switch.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i, j := 10, 20
+
+	switch i {
+	case 10:
+		fmt.Println("It is 10")
+	case 20:
+		fmt.Println("It is 20")
+	default:
+		fmt.Println("It is, what it is", i)
+	}
+
+	// Expression less...
+Fini:	
+	switch {
+	case j == 10:
+		fmt.Println("It is 10")
+		break
+	case j == 20:
+		fmt.Println("It is 20")
+		break Fini
+	}
+
+	// Fallthrough...
+	switch i {
+	case 10:
+		fallthrough
+	case 20:
+		fmt.Println("All good")
+		// break
+		// break is implicit in go 
+	default:
+		fmt.Println("No good")
+	}
+
+	// Or... merge cases
+	switch i {
+	case 10, 20:
+		fmt.Println("All good")
+	default:
+		fmt.Println("Not good")
+	}
+}
+
