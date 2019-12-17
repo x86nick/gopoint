5d56c6c7d177495c55ae6f53b8190cc7124822f1
x86nick <x86nick@gmail.com>
2019-12-17 13:26:00 -0800

    Create slicerWrange.go

---
+++ slicerWrange.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Iteration
+	s := []int{0: 1, 3: 3} // len 4 | cap 4
+
+	// This is ok
+	// NOTE: More control then range since always iterate from start of slice
+	for i := 1; i < len(s); i += 2 {
+		fmt.Printf("%d ", s[i])
+	}
+	fmt.Println()
+
+	// Or...
+	// !!!! NOTE: Range makes a copy of each element in the slice!
+	for i, v := range s {
+		fmt.Printf("[%d] %p -> %p(%d)\n", i, &v, &s[i], v)
+	}
+	fmt.Println()
+
+	// Or skip index
+	for _, v := range s {
+		fmt.Printf("%d ", v)
+	}
+	fmt.Println()
+
+	// Or skip value
+	for i := range s {
+		fmt.Printf("%d ", i)
+	}
+	fmt.Println()
+
+	// Slice as arguments
+	s1 := make([]int, 2e6)
+	process(s1)
+	fmt.Println(s1[0])
+}
+
+// Always passing a constant 3 words
+func process(s []int) {
+    s[0] = 100
+}
