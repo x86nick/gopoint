eb846595952ce577436d1e1c9adac41f170bcc3d
x86nick <x86nick@gmail.com>
2019-12-18 13:43:19 -0800

    Create channelSignal.go

---
+++ channelSignal.go
@@ -0,0 +1,23 @@
+// just want to signal, like in unix kill signal
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var c = make(chan struct{}) // empty struct, holds no memory, 
+	go func(c chan<- struct{}) { // producer, it will take empty stuct channle and do sometning
+		defer func() {
+			fmt.Println("Backup successful! (Bucket=BumblebeeTuna)")
+		}()
+		fmt.Println("DB Backup to S3...")
+		time.Sleep(2 * time.Second)
+		c <- struct{}{}  // when 11 done, pusing the empty struct to channel, signal that no data is coming
+		close(c) // close channel, it has anythign to do the resourcs
+	}(c)
+
+	_ = <-c // block here for backup to finish
+	fmt.Println("\nWe're done!")
+}
